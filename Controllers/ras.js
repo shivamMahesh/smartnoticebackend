8c1c1493216f78e5b0f3911f10146a0d6ecb1f6a
shivamMahesh <42550571+shivamMahesh@users.noreply.github.com>
2018-10-14 20:19:03 +0530

    changing r pi response

--- Controllers/ras.js
+++ Controllers/ras.js
@@ -8,6 +8,7 @@ db('image').where('fsd','<',cd)
   }).then(data=>
   {
  console.log("changing current");
+ updateUpcoming(db,res,cd);
   })
   .catch(err=>res.status(400).json(err))	
    return true;
@@ -22,6 +23,7 @@ db('image').where('fsd','>',cd)
   }).then(data=>
   {
 console.log("changing upcoming");
+updatePrevious(db,res,cd);
   })
   .catch(err=>res.status(400).json(err))
   return true;
@@ -36,6 +38,7 @@ const updatePrevious=(db,res,cd)=>
   }).then(data=>
   {
 console.log("changing previous");
+handleRas(section,res,db);
   })
   .catch(err=>res.status(400).json(err))
   return true;
@@ -51,7 +54,7 @@ db.select('description','name','fileid','fed','section','preference').from('imag
   .orderBy('preference', 'asc')
   .then(data=>
   {
-    console.log("sending to R pi");
+    console.log("sending to rpi")
   res.json(data);
   })
   .catch(err=>res.status(400).json(err))
@@ -64,22 +67,7 @@ const handleRequest=(req,res,db)=>
   var  cd=c.getFullYear()+"-"+(((c.getMonth()+1)<10)?'0':'')+(c.getMonth()+1)+"-"+((c.getDate()<10)?'0':'')+c.getDate()+" " +((c.getHours()<10)?'0':'')+c.getHours() + ":"  
                 +((c.getMinutes()<10)?'0':'')+ c.getMinutes();
 	
-if(updateCurrent(db,res,cd))
-{
-if(updatePrevious(db,res,cd))
-{
-  if(updateUpcoming(db,res,cd))
-  {
-handleRas(section,res,db);
-}
-else
-res.status(400).json('WRONG')
-}
-else
-res.status(400).json('WRONG')
-}
-else
-res.status(400).json('WRONG')
+updateCurrent(db,res,cd);
 }
 
   module.exports={
